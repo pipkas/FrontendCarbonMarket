b96849203db2127e7141646fe849d042542467b2
Софья Лопухова <s.lopukhova@g.nsu.ru>
2026-08-13 10:49:35 +0700

    change host

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // Runtime-настройка frontend-а.
 // Для деплоя на отдельный сервер замените значение на публичный адрес backend API.
-window.CARBON_MARKET_API_URL = "http://127.0.0.1:8000";
+window.CARBON_MARKET_API_URL = "https://jiffy-frostbite-creme.ngrok-free.dev";
